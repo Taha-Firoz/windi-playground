68d8a19b0b2da7ef727cef1914c2d8cf6f1555ad
Taha Firoz <i170323@nu.edu.pk>
2022-05-14 16:40:09 +0500

    Added nested elements Fixed element deletion

---
+++ src/lib/DynDomTree.svelte
@@ -0,0 +1,49 @@
+<script>
+import { currentPath } from "../stores";
+
+import { createEventDispatcher } from "svelte";
+
+
+    
+	const dispatch = createEventDispatcher();
+
+    /**
+     * @type {number[]} idx
+     */
+    export let idx
+    
+    /**
+     * @type {string} tag
+     */
+    export let tag
+    /**
+     * @type {string} classes
+     */
+    export let classes
+
+    /**
+     * @type {[string, string][]| [string, string,[]][]} children
+     */
+    export let children = []
+
+</script>
+
+
+{#if children.length === 0}
+    <svelte:element
+        this={tag}
+        class={classes}
+        on:click|stopPropagation={()=>{currentPath.set(idx)}}
+    />
+{:else}
+    <svelte:element
+    this={tag}
+    class={classes}
+    on:click|stopPropagation={()=>{currentPath.set(idx)}}
+    >
+    {#each children as child, index}
+        <svelte:self tag={child[0]} idx={[...idx, index]} classes={child[1]} children={child.length===2?[]:child[2]}/>
+    {/each}
+</svelte:element>
+
+{/if}
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,27 +1,86 @@
 <script>
-
 	import Draggable from '$lib/Draggable.svelte';
+	import DynDomTree from '$lib/DynDomTree.svelte';
 	import { clickOutside, resize } from '$lib/utils';
 	import { generateStyles } from '$lib/windirunner';
+	import { currentPath } from '../stores';
+	let default_element = ['div', 'bg-blue-500 rounded w-1/5 h-80 hover:bg-red-400'];
 
-	const default_element = ['div', 'bg-blue-500 rounded w-1/5 h-80 hover:bg-red-400'];
-	let elements = [[...default_element]];
+	let elements = [[...default_element, []]];
 	let dyStyle = null;
 	let disable_drag = false;
-	let editable_selected = null;
+
+	/**
+	 * @type {number[] | null}
+	 */
+	let elementPath = null;
+	let element = null;
+	let elementIdx = null;
+	let parent = null;
 
 	let className = '';
 	let elementType = '';
 	let clone = false;
 
 	let showDropDown = false;
+	let show_info = false;
+	let show_default = false;
+	currentPath.subscribe((path) => {
+		if (path != null) {
+			elementPath = path;
+			getElement();
+		}
+	});
 
-    let show_info = false
+	function getElement() {
+		parent = null;
+		element = null;
+		elementIdx = null;
+		for (const idx of elementPath) {
+			if (element === null) {
+				element = elements[idx];
+			} else {
+				parent = element;
+				element = element[2][idx];
+			}
+			elementIdx = idx;
+		}
+		className = element[1];
+		elementType = element[0];
+	}
+	function setElement() {
+		if (element != null) {
+			element[0] = elementType;
+			element[1] = className;
+			elements = [...elements];
+		}
+	}
+
+	function addElement(position) {
+		if (position === 'Above') {
+			parent[2].splice(elementIdx, 0, [
+				...(!clone
+					? [...default_element, []]
+					: [elementType, className, []])
+			]);
+		} else if (position === 'Inside') {
+			element[2].push([
+				...(!clone
+					? [...default_element, []]
+					: [elementType, className, []])
+			]);
+		} else if (position === 'Below') {
+			parent[2].splice(elementIdx + 1, 0, [
+				...(!clone
+					? [...default_element, []]
+					: [elementType, className, []])
+			]);
+		}
+		elements = [...elements];
+	}
 	function updateStyle() {
-		if (editable_selected !== null) {
-			// reassign the elements array with the new style
-			elements[editable_selected][0] = elementType;
-			elements[editable_selected][1] = className;
+		if (elementPath !== null) {
+			setElement();
 		}
 		let styles = generateStyles(elements.map((e) => e[1]).join(' '));
 		if (dyStyle === null) {
@@ -35,52 +94,119 @@
 </script>
 
 <span class="z-20 absolute bottom-0 right-0 flex flex-col">
-    {#if show_info}
-	<div use:clickOutside on:click_outside={()=>{show_info = false}} class="text-white rounded-lg mb-4 mr-4 bg-cool-gray-500 bg-opacity-60 p-6 min-h-50 max-w-70 border border-transparent hover:border-white shadow-lg">
-		Hi 😅 this is just a random website I made that 3 AM to test windi-css out and do quick experiments.
-        Look around and try some stuff
-	</div>
-        
-    {/if}
+	{#if show_info}
+		<div
+			use:clickOutside
+			on:click_outside={() => {
+				show_info = false;
+			}}
+			class="text-white rounded-lg mb-4 mr-4 bg-cool-gray-500 bg-opacity-60 p-6 min-h-50 max-w-70 border border-transparent hover:border-white shadow-lg"
+		>
+			Hi 😅 this is just a random website I made that 3 AM to test windi-css out and do quick
+			experiments. Look around and try some stuff
+		</div>
+	{/if}
+
+	{#if show_default}
+		<div
+			use:clickOutside
+			on:click_outside={() => {
+				show_default = false;
+			}}
+			class="text-white rounded-lg mb-4 mr-4 bg-cool-gray-500 bg-opacity-60 p-6 min-h-50 min-w-70 border border-transparent hover:border-white shadow-lg flex flex-col"
+		>
+			<div class="flex flex-col space-y-6">
+				<div class="flex flex-col">
+					<span>Classes</span>
+					<textarea
+						bind:value={default_element[1]}
+						on:resize={() => {
+							disable_drag = true;
+						}}
+						use:resize
+						on:input={updateStyle}
+						use:clickOutside
+						on:click_outside={() => (disable_drag = false)}
+						on:focus={() => (disable_drag = true)}
+						class="min-w-100 resize text-gray-600 font-semibold px-4 py-2 outline-none border-gray-400 border rounded-lg focus:border-blue-400"
+						placeholder="Enter some classes"
+					/>
+				</div>
+				<div class="flex flex-col">
+					<span>Type</span>
+					<select
+						bind:value={default_element[0]}
+						on:change={updateStyle}
+						class=" font-semibold bg-white p-4 rounded-md text-gray-600 border-gray-400 border rounded-lg focus:border-blue-400"
+					>
+						<option value="">nothing selected</option>
+						<option value="div">div</option>
+						<option value="input">input</option>
+						<option value="textarea">textarea</option>
+						<option value="button">button</option>
+					</select>
+				</div>
+			</div>
+		</div>
+	{/if}
 	<span class="flex justify-end text-xl">
+		<button
+			on:click={() => {
+				show_default = !show_default;
+			}}
+			class="h-10 w-10 rounded-lg border-transparent mb-4 mr-4 bg-cool-gray-500 bg-opacity-40 hover:bg-opacity-60 hover:text-white hover:text-opacity-40 !active:text-opacity-100 border active:border-white"
+		>
+			⚙
+		</button>
+
 		<button
 			on:click={() => {
 				elements = [[...default_element]];
 				dyStyle = null;
 				disable_drag = false;
-				editable_selected = null;
-
+				elementPath = null;
+				show_default = false;
+				default_element = ['div', 'bg-blue-500 rounded w-1/5 h-80 hover:bg-red-400', []];
 				className = '';
 				elementType = '';
 				clone = false;
-                show_info = false;
+				show_info = false;
 			}}
 			class=" h-10 w-10 rounded-lg border-transparent mb-4 mr-4 bg-cool-gray-500 bg-opacity-40 hover:bg-opacity-60 hover:text-white hover:text-opacity-40 !active:text-opacity-100 border active:border-white"
 		>
 			⭯
 		</button>
 		<button
-            on:click={() => {
-                show_info = !show_info
-            }}
+			on:click={() => {
+				show_info = !show_info;
+			}}
 			class="h-10 w-10 rounded-lg border-transparent mb-4 mr-4 bg-cool-gray-500 bg-opacity-40 hover:bg-opacity-60 hover:text-white hover:text-opacity-40 !active:text-opacity-100 border active:border-white"
 		>
 			?
 		</button>
 
-        <a href="https://github.com" aria-label="Homepage" class="flex justify-center items-center h-10 w-10 rounded-lg border-transparent mb-4 mr-4 bg-cool-gray-500 opacity-40 hover:bg-opacity-60 hover:fill-white hover:opacity-60 !active:opacity-100 border active:border-white" title="GitHub">
-            <svg aria-hidden="true" class="h-8 w-8 " version="1.1" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0 0 16 8c0-4.42-3.58-8-8-8z"></path>
-      </svg>
-      </a>
+		<a
+			href="https://github.com"
+			aria-label="Homepage"
+			class="flex justify-center items-center h-10 w-10 rounded-lg border-transparent mb-4 mr-4 bg-cool-gray-500 opacity-40 hover:bg-opacity-60 hover:fill-white hover:opacity-60 !active:opacity-100 border active:border-white"
+			title="GitHub"
+		>
+			<svg aria-hidden="true" class="h-8 w-8 " version="1.1" viewBox="0 0 16 16"
+				><path
+					fill-rule="evenodd"
+					d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0 0 16 8c0-4.42-3.58-8-8-8z"
+				/>
+			</svg>
+		</a>
 	</span>
 </span>
-{#if editable_selected !== null}
+{#if elementPath !== null}
 	<Draggable disabled={disable_drag}>
 		<div class="flex flex-col text-lg font-bold text-cool-gray-500 px-8 py-8 min-w-100">
 			<span class="flex mb-6 justify-between h-10">
 				<span class="text-2xl align-middle">Windi Editor</span>
 				<button
-					on:click={() => (editable_selected = null)}
+					on:click={() => (elementPath = null)}
 					class="w-10 h-10 bg-gray-400 text-center rounded-xl font-bold text-white hover:bg-opacity-80 active:bg-gray-600"
 				>
 					x
@@ -123,65 +249,66 @@
 				</div>
 				<span class="flex justify-between items-center">
 					<span class="flex space-x-4">
-
 						<button
-							on:click={() => {
-								elements.splice(editable_selected === 0 ? 0 : editable_selected, 0, [
-									...(!clone ? default_element : [elementType, className])
-								]);
-								elements = [...elements];
-							}}
-							class="h-15 w-15 bg-blue-500 text-white text-center text-2xl rounded-xl hover:bg-opacity-90 active:bg-blue-600"
+							disabled={parent === null}
+							on:click={() => addElement('Above')}
+							class="h-15 w-15 bg-blue-500 text-white text-center text-2xl disabled:bg-gray-400 rounded-xl hover:bg-opacity-90 active:bg-blue-600"
 						>
 							↑ +
 						</button>
 						<button
 							on:click={() => {
-								elements.splice(editable_selected, 0, [
-									...(!clone ? default_element : [elementType, className])
-								]);
-								elements = [...elements];
+								addElement('Inside');
 							}}
 							class="h-15 w-15 bg-blue-500 text-white text-center text-2xl rounded-xl hover:bg-opacity-90 active:bg-blue-600"
 						>
 							+
 						</button>
 						<button
+							disabled={parent === null}
 							on:click={() => {
-								elements.splice(editable_selected, 0, [
-									...(!clone ? default_element : [elementType, className])
-								]);
-								elements = [...elements];
+								addElement('Below');
 							}}
-							class="h-15 w-15 bg-blue-500 text-white text-center text-2xl rounded-xl hover:bg-opacity-90 active:bg-blue-600"
+							class="h-15 w-15 bg-blue-500 text-white text-center text-2xl rounded-xl disabled:bg-gray-400 hover:bg-opacity-90 active:bg-blue-600"
 						>
 							+ ↓
 						</button>
 					</span>
 					<span class="flex flex-col justify-center relative inline-block p-2">
-						<button on:click={()=>{
-							showDropDown = !showDropDown
-						}} class="w-15 h-15 hover:bg-gray-300 rounded-full active:bg-gray-400">
+						<button
+							on:click={() => {
+								showDropDown = !showDropDown;
+							}}
+							class="w-15 h-15 hover:bg-gray-300 rounded-full active:bg-gray-400"
+						>
 							⋮
 						</button>
 						{#if showDropDown}
-							<span use:clickOutside on:click_outside={()=>{showDropDown = false}} class="absolute right-0 top-0 min-h-20 w-40 bg-cool-gray-200 border border-gray-400 shadow-2xl rounded-lg flex flex-col py-4">
+							<span
+								use:clickOutside
+								on:click_outside={() => {
+									showDropDown = false;
+								}}
+								class="absolute right-0 top-0 min-h-20 w-40 bg-cool-gray-200 border border-gray-400 shadow-2xl rounded-lg flex flex-col py-4"
+							>
 								<button
+									disabled={parent === null}
 									on:click={() => {
-										showDropDown = false
-										elements = elements.filter((_, idx) => idx !== editable_selected);
-										editable_selected = null;
+										showDropDown = false;
+										elements = parent[2] = parent[2].filter(
+											(element, index) => index !== elementIdx
+										);
+										elementPath = null;
 									}}
-									class="hover:bg-gray-400 active:bg-blue-600 p-4 w-full "
+									class="disabled:bg-cool-gray-200 disabled:cursor-not-allowed hover:bg-gray-400 active:bg-blue-600 p-4 w-full "
 								>
 									delete element
 								</button>
-								<span class="w-full h-2 border-b-2 border-gray-400 mb-2"></span>
+								<span class="w-full h-2 border-b-2 border-gray-400 mb-2" />
 								<button
 									on:click={() => {
-										elementType= elements[editable_selected][0] = default_element[0];
-										className= elements[editable_selected][1] = default_element[1];
-										
+										elementType = elements[elementPath][0] = default_element[0];
+										className = elements[elementPath][1] = default_element[1];
 									}}
 									class="hover:bg-gray-400 active:bg-blue-600 p-4 w-full "
 								>
@@ -197,16 +324,5 @@
 {/if}
 
 <div class="z-0 h-screen flex flex-col justify-center items-center bg-gray-800 dots overflow-auto">
-	{#each elements as [tag, classes], index}
-		<svelte:element
-			this={tag}
-			class={classes}
-			on:click={(e) => {
-				clone = false;
-				editable_selected = index;
-				className = classes;
-				elementType = tag;
-			}}
-		/>
-	{/each}
+	<DynDomTree idx={[0]} tag={elements[0][0]} classes={elements[0][1]} children={elements[0][2]} />
 </div>
